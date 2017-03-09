14e6f830d3c239e8bd08544afde29d09b0ed718a
Rickard Sundin <rickard.sundin@postkodlotteriet.se>
2017-03-09 10:23:16 +0100

    Move mini window to other corner on mouse over
    Hold shift to be able to click mini window.

--- src/main/java/com/greatwebguy/application/MobTime.java
+++ src/main/java/com/greatwebguy/application/MobTime.java
@@ -26,86 +26,105 @@ import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 
 public class MobTime extends Application {
-	private Stage miniTimer;
-	private Stage mainStage;
-	@Override
-	public void start(Stage stage) {
-		try {
-			mainStage = stage;
-			InputStream is = getClass().getResourceAsStream("fontawesome-webfont.ttf");
-			GlyphFont fa = new FontAwesome(is);
-			GlyphFontRegistry.register(fa);
-			Parent root = FXMLLoader.load(getClass().getResource("application.fxml"));
-			stage.setTitle("MobTime");
-			stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
-			openMiniTimer();
-			
-			stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-		        @Override
-		        public void handle(final WindowEvent event) {
-		        	miniTimer.close();
-		        }
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private Stage miniTimer;
+    private Stage mainStage;
+    private boolean isBottomRight = true;
+    private int height = 35;
+    private int width = 50;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	private void openMiniTimer() {
-		if (miniTimer == null) {
-			int height = 35;
-			int width = 50;
-			Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-			miniTimer = new Stage();
-			miniTimer.initStyle(StageStyle.TRANSPARENT);
-			miniTimer.setX(screenBounds.getMinX() + screenBounds.getWidth() - width);
-			miniTimer.setY(screenBounds.getMinY() + screenBounds.getHeight() - height);
-			Label turn = new Label();
-			turn.setPrefWidth(width);
-			turn.setPrefHeight(height - 25);
-			turn.setTextAlignment(TextAlignment.CENTER);
-			turn.setAlignment(Pos.CENTER);
-			turn.setStyle("-fx-background-color: #000000; -fx-text-fill: white; -fx-font-size: 10px;");
-			turn.textProperty().bind(Settings.instance().userName);
-			Label timer = new Label();
-			timer.setPrefWidth(width);
-			timer.setPrefHeight(height - 10);
-			timer.textProperty().bind(TimeController.timeMinutes);
-			timer.setTextAlignment(TextAlignment.CENTER);
-			timer.setAlignment(Pos.CENTER);
-			timer.setTextFill(Paint.valueOf("white"));
-			timer.styleProperty().bind(TimeController.paneColor);
-			VBox box = new VBox();
-			box.setAlignment(Pos.CENTER);
-			box.getChildren().add(turn);
-			box.getChildren().add(timer);
-			box.setCenterShape(true);
-			final Scene scene = new Scene(box, width, height);
-			scene.setFill(Color.TRANSPARENT);
-			miniTimer.setScene(scene);
-			miniTimer.setAlwaysOnTop(true);
-			miniTimer.show();
+    @Override
+    public void start(Stage stage) {
+        try {
+            mainStage = stage;
+            InputStream is = getClass().getResourceAsStream("fontawesome-webfont.ttf");
+            GlyphFont fa = new FontAwesome(is);
+            GlyphFontRegistry.register(fa);
+            Parent root = FXMLLoader.load(getClass().getResource("application.fxml"));
+            stage.setTitle("MobTime");
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            openMiniTimer();
 
-			box.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-				@Override
-				public void handle(MouseEvent event) {
-					showMainWindow();
-				}
-			});
-		}
-	}
-	
-	public void showMainWindow() {
-		Stage window = (Stage) mainStage.getScene().getWindow();
-		window.toFront();
-		window.requestFocus();
-	}
-	
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(final WindowEvent event) {
+                    miniTimer.close();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void openMiniTimer() {
+        if (miniTimer == null) {
+            Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+            miniTimer = new Stage();
+            miniTimer.initStyle(StageStyle.TRANSPARENT);
+            miniTimer.setX(screenBounds.getMinX() + screenBounds.getWidth() - width);
+            miniTimer.setY(screenBounds.getMinY() + screenBounds.getHeight() - height);
+            Label turn = new Label();
+            turn.setPrefWidth(width);
+            turn.setPrefHeight(height - 25);
+            turn.setTextAlignment(TextAlignment.CENTER);
+            turn.setAlignment(Pos.CENTER);
+            turn.setStyle("-fx-background-color: #000000; -fx-text-fill: white; -fx-font-size: 10px;");
+            turn.textProperty().bind(Settings.instance().userName);
+            Label timer = new Label();
+            timer.setPrefWidth(width);
+            timer.setPrefHeight(height - 10);
+            timer.textProperty().bind(TimeController.timeMinutes);
+            timer.setTextAlignment(TextAlignment.CENTER);
+            timer.setAlignment(Pos.CENTER);
+            timer.setTextFill(Paint.valueOf("white"));
+            timer.styleProperty().bind(TimeController.paneColor);
+            VBox box = new VBox();
+            box.setAlignment(Pos.CENTER);
+            box.getChildren().add(turn);
+            box.getChildren().add(timer);
+            box.setCenterShape(true);
+            final Scene scene = new Scene(box, width, height);
+            scene.setFill(Color.TRANSPARENT);
+            miniTimer.setScene(scene);
+            miniTimer.setAlwaysOnTop(true);
+            miniTimer.show();
+
+            box.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    showMainWindow();
+                }
+            });
+            box.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if (!event.isShiftDown()) {
+                        moveToOtherCorner();
+                    }
+                }
+            });
+        }
+    }
+
+    private void moveToOtherCorner() {
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        if (isBottomRight) {
+            miniTimer.setX(screenBounds.getMinX());
+        } else {
+            miniTimer.setX(screenBounds.getMinX() + screenBounds.getWidth() - width);
+        }
+        isBottomRight = !isBottomRight;
+    }
+
+    public void showMainWindow() {
+        Stage window = (Stage) mainStage.getScene().getWindow();
+        window.toFront();
+        window.requestFocus();
+    }
 }
